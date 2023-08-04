ad602d5fdb2d2269bcd9c8fdf05ebb3695612a68
Matthäus G. Chajdas <dev@anteru.net>
2023-08-04 19:22:43 +0200

    Don't use sudo in zfs-free.
    This hasn't been needed for quite some time.

--- zfs-free.sh
+++ zfs-free.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUR_PATH=`pwd`
 
-USED=$((`sudo zfs get -o value -Hp used $CUR_PATH` / 1024)) > /dev/null
-AVAIL=$((`sudo zfs get -o value -Hp available $CUR_PATH` / 1024)) > /dev/null
+USED=$((`zfs get -o value -Hp used $CUR_PATH` / 1024)) > /dev/null
+AVAIL=$((`zfs get -o value -Hp available $CUR_PATH` / 1024)) > /dev/null
 TOTAL=$(($USED+$AVAIL)) > /dev/null
 echo $TOTAL $AVAIL 1024
