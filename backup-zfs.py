c581e71df3d1683222a76a76fb7b8b6dd711e373
Matthäus G. Chajdas <dev@anteru.net>
2023-08-13 18:41:31 +0200

    Add a backup script.

---
+++ backup-zfs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: BSD-2-Clause
+import subprocess
+import argparse
+import json
+import zfs
+
+__version__ = '1.0'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'))
+    parser.add_argument('--dry-run', action='store_true')
+    parser.add_argument('TARGET_POOL')
+
+    args = parser.parse_args()
+
+    config = {
+        'filesystems': ['tank/Default'],
+        'backup-prefix': 'backup_'
+    }
+
+    if args.config:
+        config = json.load(args.config)
+
+    for fs in config['filesystems']:
+        for snapshot in zfs.GetSnapshots(fs, config['backup-prefix']):
+            short_name = snapshot.Timestamp.strftime(
+                config['backup-prefix'] + '%Y-%m-%d')
+            target_path = args.TARGET_POOL + '/' + \
+                snapshot.Path.replace('/', '_')
+            print(f"Creating backup of '{snapshot.Path}@{snapshot.Name}' to "
+                  f"'{target_path}'")
+            
+            call = ['zfs', 'send', f"{snapshot.Path}@{snapshot.Name}",
+                    '|',
+                    'zfs', 'recv', '-Fuv', target_path
+            ]
+            if args.dry_run:
+                print(' '.join(call))
+            else:
+                subprocess.call(' '.join(call), shell=True)
